f0cfa64ce43a6ac9dd64c3d052b4e30ddfa26c6a
thejan14 <jan.oberpichler@googlemail.com>
2022-12-03 10:21:26 +0100

    solution day 3 part 1

---
+++ day03/part1.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const fs = require("fs");
+let { performance } = require("perf_hooks");
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+const execStart = performance.now();
+
+/* begin solution */
+
+const answer = input
+  .split("\n")
+  .map((line) => [
+    new Set(line.substring(0, line.length / 2)),
+    new Set(line.substring(line.length / 2, line.length)),
+  ])
+  .map(
+    ([compartmentA, compartmentB]) =>
+      [...compartmentA].filter((item) => compartmentB.has(item))[0]
+  )
+  .map((item) => getItemPriority(item))
+  .reduce((acc, priority) => acc + priority, 0);
+
+function getItemPriority(item) {
+  if (item === item.toUpperCase()) {
+    return item.charCodeAt(0) - 65 + 27;
+  } else {
+    return item.charCodeAt(0) - 97 + 1;
+  }
+}
+
+/* end solution */
+
+const execEnd = performance.now();
+const micros = (execEnd - execStart) * 1000;
+console.log(`${answer} (${micros.toFixed(2)} µs)`);
