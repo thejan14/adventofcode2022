f345456bfa1ba34c60fed6441acccb5071536c8b
thejan14 <jan.oberpichler@googlemail.com>
2022-12-07 12:28:48 +0100

    solution day 6 part 1

---
+++ day06/part1.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+let { performance } = require("perf_hooks");
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+const execStart = performance.now();
+
+/* begin solution */
+
+let answer = 0;
+const markerSize = 4;
+for (let i = markerSize; i < input.length; i++) {
+  const buffer = new Set(input.substring(i - markerSize, i));
+  if (buffer.size === markerSize) {
+    answer = i;
+    break;
+  }
+}
+
+/* end solution */
+
+const execEnd = performance.now();
+const micros = (execEnd - execStart) * 1000;
+console.log(`${answer} (${micros.toFixed(2)} µs)`);
