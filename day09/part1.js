f8f0cd698b709030e24b3f3e563ab60ac73949a7
thejan14 <jan.oberpichler@googlemail.com>
2022-12-09 14:30:14 +0100

    refactor part 1 with updated movetail

--- day09/part1.js
+++ day09/part1.js
@@ -14,7 +14,7 @@ const instructions = input
 const visited = new Set();
 const head = [0, 0];
 const tail = [0, 0];
-for (const [_, direction, distance] of instructions) {
+for (const [, direction, distance] of instructions) {
   let step;
   let axis;
   let goal;
@@ -30,19 +30,24 @@ for (const [_, direction, distance] of instructions) {
 
   while (head[axis] !== goal) {
     head[axis] += step;
-    movetail(head, tail, axis);
+    movetail(head, tail);
     visited.add(`${tail}`);
   }
 }
 
-function movetail(head, tail, axis) {
-  if (head[axis] > tail[axis] + 1) {
-    tail[(axis + 1) % 2] = head[(axis + 1) % 2];
-    tail[axis] = head[axis] - 1;
-  }
-  if (head[axis] < tail[axis] - 1) {
-    tail[(axis + 1) % 2] = head[(axis + 1) % 2];
-    tail[axis] = head[axis] + 1;
+function movetail(head, tail) {
+  const delta = [head[0] - tail[0], head[1] - tail[1]];
+  const absDelta = [Math.abs(delta[0]), Math.abs(delta[1])];
+  const moveAxis = absDelta[0] > absDelta[1] ? 0 : 1;
+  const otherAxis = (moveAxis + 1) % 2;
+
+  if (absDelta[moveAxis] > 1) {
+    if (absDelta[otherAxis] > 0) {
+      tail[moveAxis] += Math.sign(delta[moveAxis]);
+      tail[otherAxis] += Math.sign(delta[otherAxis]);
+    } else {
+      tail[moveAxis] += Math.sign(delta[moveAxis]);
+    }
   }
 }
 
